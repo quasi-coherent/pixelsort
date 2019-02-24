9401acab70e0a28b63111da6c56509ba15eb4924
Daniel Donohue <ddonohue@earnestresearch.com>
2019-02-24 18:42:59 -0500

    more featureful

--- Main.hs
+++ Main.hs
@@ -18,11 +18,13 @@ import           Options.Applicative
 main :: IO ()
 main = do
   opts@Opts {..} <- execParser optsParser
-  orig <- either (error "Couldn't read image") convertRGB8 <$> readImage oImgPath
+  orig <- either (error "Couldn't read image") convertRGBA8 <$> readImage oImgPath
   when oRed (writePng ("sorted-r-" <> oImgPath) $ makeSortedImage compareRed orig)
   when oGreen (writePng ("sorted-g-" <> oImgPath) $ makeSortedImage compareGreen orig)
   when oBlue (writePng ("sorted-b-" <> oImgPath) $ makeSortedImage compareBlue orig)
+  when oAlpha (writePng ("sorted-a-" <> oImgPath) $ makeSortedImage compareAlpha orig)
   when oLuminance (writePng ("sorted-L-" <> oImgPath) $ makeSortedImage compareLuminance orig)
+  when oHue (writePng ("sorted-H-" <> oImgPath) $ makeSortedImage compareHue orig)
   where
     optsParser = info (helper <*> parseOpts) (header "pixelsort")
 
@@ -31,7 +33,9 @@ main = do
       <*> switch (short 'r' <> help "Sort by red")
       <*> switch (short 'g' <> help "Sort by green")
       <*> switch (short 'b' <> help "Sort by blue")
+      <*> switch (short 'a' <> help "Sort by alpha")
       <*> switch (short 'L' <> help "Sort by luminance")
+      <*> switch (short 'H' <> help "Sort by hue")
 
 
 -- | CLI.
@@ -40,12 +44,14 @@ data Opts = Opts
   , oRed       :: Bool
   , oGreen     :: Bool
   , oBlue      :: Bool
+  , oAlpha     :: Bool
   , oLuminance :: Bool
+  , oHue       :: Bool
   } deriving (Eq, Show)
 
 
 -- | Sort the image with the given ordering.
-makeSortedImage :: PixelOrdering -> Image PixelRGB8 -> Image PixelRGB8
+makeSortedImage :: PixelOrdering -> Image PixelRGBA8 -> Image PixelRGBA8
 makeSortedImage f Image {..} = runST $ do
   mimg <- newMutableImage imageWidth imageHeight
   go 0 imageData mimg
@@ -53,10 +59,10 @@ makeSortedImage f Image {..} = runST $ do
     go r d mimg
       | r >= imageHeight = unsafeFreezeImage mimg
       | otherwise = do
-          let row = makeRow (3*imageWidth) (VS.take (3*imageWidth) d)
+          let row = makeRow (4*imageWidth) (VS.take (4*imageWidth) d)
               sortedRow = V.modify (VA.sortBy f) row
           writeRow 0 r sortedRow mimg
-          go (r+1) (VS.drop (3*imageWidth) d) mimg
+          go (r+1) (VS.drop (4*imageWidth) d) mimg
 
     writeRow c r v mimg
       | c >= imageWidth = unsafeFreezeImage mimg
@@ -65,19 +71,19 @@ makeSortedImage f Image {..} = runST $ do
           writeRow (c+1) r v mimg
 
 
--- | Make one row of 'PixelRGB8's from the image's raw representation.
-makeRow :: Int -> VS.Vector Word8 -> V.Vector PixelRGB8
+-- | Make one row of 'PixelRGBA8's from the image's raw representation.
+makeRow :: Int -> VS.Vector Word8 -> V.Vector PixelRGBA8
 makeRow = go V.empty
   where
     go !acc !w !d
       | w == 0    = acc
-      | otherwise = go (acc V.++ makePixel (VS.take 3 d)) (w-3) (VS.drop 3 d)
+      | otherwise = go (acc V.++ makePixel (VS.take 4 d)) (w-4) (VS.drop 4 d)
 
-    makePixel d = V.singleton (PixelRGB8 (d VS.! 0) (d VS.! 1) (d VS.! 2))
+    makePixel d = V.singleton (PixelRGBA8 (d VS.! 0) (d VS.! 1) (d VS.! 2) (d VS.! 3))
 
 
 -- | How to arrange pixels.
-type PixelOrdering = PixelRGB8 -> PixelRGB8 -> Ordering
+type PixelOrdering = PixelRGBA8 -> PixelRGBA8 -> Ordering
 
 
 -- | Composition of user sort choices.
@@ -93,17 +99,22 @@ userSortChoice Opts {..} = if length userOpts /= 1
 
 -- | Which pixel is more red.
 compareRed :: PixelOrdering
-compareRed (PixelRGB8 r1 _ _) (PixelRGB8 r2 _ _) = compare r1 r2
+compareRed (PixelRGBA8 r1 _ _ _) (PixelRGBA8 r2 _ _ _) = compare r1 r2
 
 
 -- | Which pixel is more green.
 compareGreen :: PixelOrdering
-compareGreen (PixelRGB8 _ g1 _) (PixelRGB8 _ g2 _) = compare g1 g2
+compareGreen (PixelRGBA8 _ g1 _ _) (PixelRGBA8 _ g2 _ _) = compare g1 g2
 
 
 -- | Which pixel is more blue.
 compareBlue :: PixelOrdering
-compareBlue (PixelRGB8 _ _ b1) (PixelRGB8 _ _ b2) = compare b1 b2
+compareBlue (PixelRGBA8 _ _ b1 _) (PixelRGBA8 _ _ b2 _) = compare b1 b2
+
+
+-- | Which pixel is more opaque.
+compareAlpha :: PixelOrdering
+compareAlpha (PixelRGBA8 _ _  _ a1) (PixelRGBA8 _ _ _ a2) = compare a1 a2
 
 
 -- | Which pixel is brighter.
@@ -112,5 +123,15 @@ compareBlue (PixelRGB8 _ _ b1) (PixelRGB8 _ _ b2) = compare b1 b2
 compareLuminance :: PixelOrdering
 compareLuminance a b = compare (relativeLuminance a) (relativeLuminance b)
   where
-    relativeLuminance (PixelRGB8 r g b)
+    relativeLuminance (PixelRGBA8 r g b _)
       = 0.2126 * fromIntegral r + 0.7152 * fromIntegral g + 0.0722 * fromIntegral b
+
+
+-- | Which pixel is more hue-y.
+--
+-- https://en.wikipedia.org/wiki/Hue
+compareHue :: PixelOrdering
+compareHue a b = compare (hue a) (hue b)
+  where
+    hue (PixelRGBA8 r g b _) =
+      atan2 (sqrt 3 * (fromIntegral g - fromIntegral b)) (2 * fromIntegral r - fromIntegral g - fromIntegral b)
