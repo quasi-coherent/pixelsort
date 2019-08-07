219de28b8731df969df5f887fa72ed86ef497fda
Daniel Donohue <ddonohue@earnestresearch.com>
2019-08-07 00:48:03 -0400

    random acts of randomness

--- Main.hs
+++ Main.hs
@@ -14,6 +14,7 @@ import qualified Data.Vector.Storable as VS
 import           Data.Word (Word8)
 import           Options.Applicative
 import           System.FilePath.Lens
+import           System.Random
 
 
 main :: IO ()
@@ -29,6 +30,8 @@ main = do
   when oAverage (writePng (baseDir <> "/sorted-M-" <> fileName) $ makeSortedImage compareAverage orig)
   when oLuminance (writePng (baseDir <> "/sorted-L-" <> fileName) $ makeSortedImage compareLuminance orig)
   when oHue (writePng (baseDir <> "/sorted-H-" <> fileName) $ makeSortedImage compareHue orig)
+  when oRandom $ compareRandomly >>= \ord ->
+    writePng (baseDir <> "/sorted-R-" <> fileName) $ makeSortedImage ord orig
   where
     optsParser = info (helper <*> parseOpts) (header "pixelsort")
 
@@ -41,6 +44,7 @@ main = do
       <*> switch (short 'M' <> help "Sort by average of pixel values")
       <*> switch (short 'L' <> help "Sort by luminance")
       <*> switch (short 'H' <> help "Sort by hue")
+      <*> switch (short 'R' <> help "Sort randomly")
 
 
 -- | CLI.
@@ -53,6 +57,7 @@ data Opts = Opts
   , oAverage   :: Bool
   , oLuminance :: Bool
   , oHue       :: Bool
+  , oRandom    :: Bool
   } deriving (Eq, Show)
 
 
@@ -136,3 +141,30 @@ compareHue a b = compare (hue a) (hue b)
   where
     hue (PixelRGBA8 r g b _) =
       atan2 (sqrt 3 * (fromIntegral g - fromIntegral b)) (2 * fromIntegral r - fromIntegral g - fromIntegral b)
+
+
+-- | Random comparison.
+compareRandomly :: IO PixelOrdering
+compareRandomly = do
+  n1 <- randomRIO (0, 3)
+  n2 <- randomRIO (0, 3)
+  return $ compare' n1 n2
+  where
+    compare' :: Int -> Int -> PixelRGBA8 -> PixelRGBA8 -> Ordering
+    compare' 0 0 (PixelRGBA8 r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) = compare r1 r2
+    compare' 0 1 (PixelRGBA8 r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) = compare r1 g2
+    compare' 0 2 (PixelRGBA8 r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) = compare r1 b2
+    compare' 0 3 (PixelRGBA8 r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) = compare r1 a2
+    compare' 1 0 (PixelRGBA8 r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) = compare g1 r2
+    compare' 1 1 (PixelRGBA8 r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) = compare g1 g2
+    compare' 1 2 (PixelRGBA8 r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) = compare g1 b2
+    compare' 1 3 (PixelRGBA8 r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) = compare g1 a2
+    compare' 2 0 (PixelRGBA8 r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) = compare b1 r2
+    compare' 2 1 (PixelRGBA8 r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) = compare b1 g2
+    compare' 2 2 (PixelRGBA8 r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) = compare b1 b2
+    compare' 2 3 (PixelRGBA8 r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) = compare b1 a2
+    compare' 3 0 (PixelRGBA8 r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) = compare a1 r2
+    compare' 3 1 (PixelRGBA8 r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) = compare a1 g2
+    compare' 3 2 (PixelRGBA8 r1 gf b1 a1) (PixelRGBA8 r2 g2 b2 a2) = compare a1 b2
+    compare' 3 3 (PixelRGBA8 r1 gf b1 a1) (PixelRGBA8 r2 g2 b2 a2) = compare a1 a2
+    compare' _ _ _ _ = error "The impossible has happened"
